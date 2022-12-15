585d4aabdb3da332712833c60f10abf70f550814
BaianorASR <reis.adson2@outlook.com>
2022-12-14 23:21:32 -0300

    test: notification content length error

--- src/app/entities/notification-content.spec.ts
+++ src/app/entities/notification-content.spec.ts
@@ -5,4 +5,16 @@ describe('NotificationContent', () => {
     const content = new NotificationContent('Hello World');
     expect(content.value).toBeTruthy();
   });
+
+  it('should not be able to create a notification content with less 5 characters', () => {
+    expect(() => {
+      new NotificationContent('qqq');
+    }).toThrow();
+  });
+
+  it('should not be able to create a notification content with more 240 characters', () => {
+    expect(() => {
+      new NotificationContent('1'.repeat(241));
+    }).toThrow();
+  });
 });
