8985cbe40989cbb96f0a7904e5987ebf88b29eae
BaianorASR <reis.adson2@outlook.com>
2022-12-14 23:07:48 -0300

    feat: start DDD Notification

---
+++ src/app/entities/notification.entity.ts
@@ -0,0 +1,49 @@
+import { NotificationContent } from './notification-content';
+
+export interface NotificationProps {
+  recipientId: string;
+  content: NotificationContent;
+  category: string;
+  readAt?: Date | null;
+  createdAt: Date;
+}
+
+export class Notification {
+  constructor(private readonly props: NotificationProps) {}
+
+  public get recipientId(): string {
+    return this.props.recipientId;
+  }
+
+  public set recipientId(uuid: string) {
+    this.props.recipientId = uuid;
+  }
+
+  public get content(): NotificationContent {
+    return this.props.content;
+  }
+
+  public set content(content: NotificationContent) {
+    this.props.content = content;
+  }
+
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  public set readAt(date: Date | null | undefined) {
+    this.props.readAt = date;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
+}
