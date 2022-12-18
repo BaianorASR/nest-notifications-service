4187cf733879399598e4dfe9f2e39400c773d308
BaianorASR <reis.adson2@outlook.com>
2022-12-18 18:36:29 -0300

    feat: implements controller

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,13 +1,60 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 
 import { CreateNotificationsDto } from '../dtos/create-notifications.dto';
 import { NotificationViewModel } from '../view-models/notification.view-model';
 
 import { SendNotification } from '@app/use-cases/send-notification.use-case';
+import { CancelNotificationUseCase } from '@app/use-cases/cancel-notification.use-case';
+import { CounterRecipientNotificationsUseCase } from '@app/use-cases/count-recipient-notifications.use-case';
+import { ReadNotificationUseCase } from '@app/use-cases/read-notification.use-case';
+import { UnreadNotificationUseCase } from '@app/use-cases/unread-notification.use-case';
+import { GetRecipientNotificationsUseCase } from '@app/use-cases/get-recipient-notifications.use-case';
 
 @Controller()
 export class NotificationsController {
-  constructor(private readonly sendNotification: SendNotification) {}
+  constructor(
+    private readonly sendNotification: SendNotification,
+    private readonly cancelNotificationUseCase: CancelNotificationUseCase,
+    private readonly readNotificationUseCase: ReadNotificationUseCase,
+    private readonly unreadNotificationUseCase: UnreadNotificationUseCase,
+    private readonly counterRecipientNotificationsUseCase: CounterRecipientNotificationsUseCase,
+    private readonly getFromRecipientUseCase: GetRecipientNotificationsUseCase,
+  ) {}
+
+  @Get('count/from/:recipientId')
+  async countFromRecipient(@Param('recipientId') recipientId: string) {
+    const { count } = await this.counterRecipientNotificationsUseCase.execute({
+      recipientId,
+    });
+
+    return { count };
+  }
+
+  @Get('from/:recipientId')
+  async getFromRecipient(@Param('recipientId') recipientId: string) {
+    const { notifications } = await this.getFromRecipientUseCase.execute({
+      recipientId,
+    });
+
+    return {
+      notifications: notifications.map(NotificationViewModel.toHTTP),
+    };
+  }
+
+  @Patch(':notificationId/cancel')
+  async cancel(@Param('notificationId') notificationId: string) {
+    return this.cancelNotificationUseCase.execute({ notificationId });
+  }
+
+  @Patch(':notificationId/read')
+  async read(@Param('notificationId') notificationId: string) {
+    return this.readNotificationUseCase.execute({ notificationId });
+  }
+
+  @Patch(':notificationId/unread')
+  async unread(@Param('notificationId') notificationId: string) {
+    return this.unreadNotificationUseCase.execute({ notificationId });
+  }
 
   @Post()
   async create(
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -5,10 +5,22 @@ import { DatabaseModule } from '../database/database.module';
 import { NotificationsController } from './controllers/notifications.controller';
 
 import { SendNotification } from '@app/use-cases/send-notification.use-case';
+import { CancelNotificationUseCase } from '@app/use-cases/cancel-notification.use-case';
+import { CounterRecipientNotificationsUseCase } from '@app/use-cases/count-recipient-notifications.use-case';
+import { ReadNotificationUseCase } from '@app/use-cases/read-notification.use-case';
+import { UnreadNotificationUseCase } from '@app/use-cases/unread-notification.use-case';
+import { GetRecipientNotificationsUseCase } from '@app/use-cases/get-recipient-notifications.use-case';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotificationUseCase,
+    CounterRecipientNotificationsUseCase,
+    ReadNotificationUseCase,
+    UnreadNotificationUseCase,
+    GetRecipientNotificationsUseCase,
+  ],
 })
 export class HttpModule {}
